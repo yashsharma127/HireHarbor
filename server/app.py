9d728bedae2eac57954b62ed7b95b352632802c9
yashsharma127 <yashshar.2323@gmail.com>
2023-11-26 23:12:12 +0530

    adding the backend code for get and post

--- server/app.py
+++ server/app.py
@@ -1,9 +1,58 @@
-from flask import Flask 
- 
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    Title = db.Column(db.String(80), unique=True, nullable=False)
+    Location = db.Column(db.String(120), unique=True, nullable=False)
+    Salary = db.Column(db.Integer, unique=True, nullable=False)
+
+    def json(self):
+        return {'id': self.id,'Title': self.Title, 'Location': self.Location, 'Salary': self.Salary}
+
+db.create_all()
+
+#create a test route
+@app.route('/test', methods=['GET'])
+def test():
+  return make_response(jsonify({'message': 'test route'}), 200)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+# create a user
+@app.route('/users', methods=['POST'])
+def create_user():
+  try:
+    data = request.get_json()
+    new_user = User(Title=data['Title'], Location=data['Location'], Salary=data['Salary'])
+    db.session.add(new_user)
+    db.session.commit()
+    return make_response(jsonify({'message': 'user created'}), 201)
+  except e:
+    return make_response(jsonify({'message': 'error creating user'}), 500)
 
+# get all users
+@app.route('/users', methods=['GET'])
+def get_users():
+  try:
+    users = User.query.all()
+    return make_response(jsonify([user.json() for user in users]), 200)
+  except e:
+    return make_response(jsonify({'message': 'error getting users'}), 500)
+
+# get a user by id
+@app.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+  try:
+    user = User.query.filter_by(id=id).first()
+    if user:
+      return make_response(jsonify({'user': user.json()}), 200)
+    return make_response(jsonify({'message': 'user not found'}), 404)
+  except e:
+    return make_response(jsonify({'message': 'error getting user'}), 500)
