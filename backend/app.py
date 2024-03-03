ebee316d30257b06ff146b83bc1e40c067cfcd2e
YASH SHARMA <yashshar.2323@gmail.com>
2024-03-03 16:57:19 +0530

    backend code to create,delete,update and get data

--- backend/app.py
+++ backend/app.py
@@ -18,3 +18,70 @@ def json(self):
     return {'id': self.id,  'name': self.name, 'email': self.email}
     
 db.create_all()
+
+@app.route('/test', methods=["GET"])
+def test():
+    return  "It works!"
+
+@app.route("/api/flask/users", methods=["POST"])
+def create_user():
+    try:
+        data =  request.get_json()
+        new_user = User(name=data["name"], email=data["email"])
+        db.session.add(new_user)
+        db.session.commit()
+
+        return jsonify({
+            'id': new_user.id,
+            'name': new_user.name,
+            'email': new_user.email
+        }), 201
+    
+    except Exception as e:
+        return make_response(jsonify({'message': 'error creating user', 'error': str(e)}), 500)
+    
+@app.route('/api/flask/users', methods=['GET'])
+def get_users():
+    try:
+        users = User.query.all()
+        users_data = [{'id':user.id, 'name':user.name, 'email':user.email} for user in users]
+        return jsonify(users_data), 200
+    except Exception as e:
+        return make_response(jsonify({'message':'error getting users', 'error': str(e)}), 500)
+    
+@app.route('/api/flask/users/<id>', methods=['GET'])
+def get_user(id):
+    try:
+        user = User.query.filter_by(id=id).first()
+        if user:
+            return make_response (jsonify({'user': user.json()}), 200)
+        return make_response(jsonify({'message': 'user not found'}), 404)
+    except Exception as e:
+        return make_response(jsonify({'message': 'error getting user', 'error': str(e)}), 500)
+    
+@app.route('/api/flask/users/<id>',methods=['PUT'])
+def update_user(id):
+    try:
+        user = User.query.filter_by(id=id).first()
+        if user:
+            data = request.get_json()
+            user.name = data['name']
+            user.email = data['email']
+            db.session.commit()
+            return make_response(jsonify({'message': 'user updated'}), 200)
+        return make_response(jsonify({'message': 'user not found'}), 404)
+    except Exception as e:
+        return make_response(jsonify({'message': 'error updating user','error':str(e)}), 500)
+
+@app.route('/api/flask/users/<id>', methods=['DELETE'])
+def delete_user(id):
+    try:
+        user = User.query.filter_by(id=id).first()
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            return make_response(jsonify({'message': 'user deleted'}, 200))
+        return make_response(jsonify({'message': 'iser not found'}), 404)
+    except Exception as e:
+        return make_response(jsonify({'message':'error deleting user', 'error': str(e)}), 500)
+    
